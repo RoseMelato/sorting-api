711c979f89fcb46917014a1f6f7236f8b4b5aa5f
Rose Melato <rosemelato25@gmail.com>
2026-02-28 13:53:04 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const app = express();
+
+//Middleware to read JSON
+app.use(express.json());
+
+//POST endpoint
+app.post("/sort-word", (req,res)=>{
+    const data = req.body?.data;
+    
+    //Validation
+    if(!data || typeof data != "string"){
+        return res,status(400).json({
+            error: "Invalid input. 'data' must be a string"
+    });}
+    
+    //convert string to array
+    const sortedChar = data
+        .split("")
+    //sort alaphabetically
+        .sort((a,b)=> a.localeCompare(b));
+
+    //Response
+        res.json({
+     word: sortedChar
+    });
+});
+
+//Start server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`server running on port ${PORT}`);
+});
